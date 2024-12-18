efa97e442f7d604729222ce14ca7e1e810482070
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2024-12-18 17:58:24 +1000

    18.12

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -25,14 +25,13 @@ export enum AppRoute {
 }
 
 export const CityMap = {
-  Paris: {name: CitiesName.Paris, location: {latitude: 48.8566, longitude: 2.3522, zoom: 10}},
-  Cologne: {name: CitiesName.Cologne, location: {latitude: 50.935173, longitude: 6.953101, zoom: 10}},
-  Brussels: {name: CitiesName.Brussels, location: {latitude: 50.8476, longitude: 4.3572, zoom: 10}},
-  Amsterdam: {name: CitiesName.Amsterdam, location: {latitude: 52.3676, longitude: 4.9041, zoom: 10}},
-  Hamburg: {name: CitiesName.Hamburg, location: {latitude: 53.5488, longitude: 9.9872, zoom: 10}},
-  Dusseldorf: {name: CitiesName.Dusseldorf, location: {latitude: 51.2277, longitude: 6.7735, zoom: 10}},
+  Paris: {name: CitiesName.Paris, location: {latitude: 48.8566, longitude: 2.3522, zoom: 10}, slug: '/paris'},
+  Cologne: {name: CitiesName.Cologne, location: {latitude: 50.935173, longitude: 6.953101, zoom: 10}, slug: '/cologne'},
+  Brussels: {name: CitiesName.Brussels, location: {latitude: 50.8476, longitude: 4.3572, zoom: 10}, slug: '/brussels'},
+  Amsterdam: {name: CitiesName.Amsterdam, location: {latitude: 52.3676, longitude: 4.9041, zoom: 10}, slug: '/amsterdam'},
+  Hamburg: {name: CitiesName.Hamburg, location: {latitude: 53.5488, longitude: 9.9872, zoom: 10}, slug: '/hamburg'},
+  Dusseldorf: {name: CitiesName.Dusseldorf, location: {latitude: 51.2277, longitude: 6.7735, zoom: 10}, slug: '/dusseldorf'},
 } as const;
-
 
 export const DEFAULT_CITY = CitiesName.Paris;
 
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -2,14 +2,13 @@ function spaceToUnderscore(str: string): string {
   return str.replace(/ /g, '_');
 }
 
-const randomInteger = (min: number, max: number) => {
+const randomInteger = (min: number, max: number):number => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const randomElement = <Element> (array: Element[] | readonly Element[]) =>
-array[randomInteger(0, array.length - 1)]
 
-export {spaceToUnderscore, randomElement, randomInteger};
+
+export {spaceToUnderscore, randomInteger};
