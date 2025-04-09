8f56a3d5238ac6aff30e9c07e146238bf2b6e58e
Eugene Shlyk <serpupstars@gmail.com>
2025-04-10 07:29:04 +1000

    10 утро

--- src/components/offer-card/offer-card.tsx
+++ src/components/offer-card/offer-card.tsx
@@ -39,7 +39,8 @@ function OfferCard({
   variant,
   size,
 }: OfferCardProps): JSX.Element {
-  console.log(offer.isFavorite);
+  const isFavorite = offer.isFavorite;
+  // console.log(offer.isFavorite);
   return (
     <article className={clsx(variant && `${variant}__card`, 'place-card')}>
       {
@@ -68,7 +69,7 @@ function OfferCard({
             <b className="place-card__price-value">&euro;{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <OfferFavoriteButton dimension={smallButtonFavoriteDimension} isOfferPageBookmark={false}></OfferFavoriteButton>
+          <OfferFavoriteButton isFavorite={isFavorite} dimension={smallButtonFavoriteDimension} isOfferPageBookmark={false}></OfferFavoriteButton>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
