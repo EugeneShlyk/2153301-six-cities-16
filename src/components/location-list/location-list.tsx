0bbbf158f32646165c8b47baa22b632eedf77ca8
Eugene Shlyk <serpupstars@gmail.com>
2025-07-04 07:23:13 +1000

    04.07 утро

--- src/components/location-list/location-list.tsx
+++ src/components/location-list/location-list.tsx
@@ -1,21 +1,22 @@
 import {Link} from 'react-router-dom';
-import {AppRoute} from '@constants';
+import {AppRoute, CitiesName} from '@constants';
 import clsx from 'clsx';
 import {CITIES} from '@constants';
 import {useAppSelector} from '../../store/hooks/useAppSelector.ts';
-import {offersSelector} from '../../store/slices/offers';
+import {offersAction, offersSelector} from '../../store/slices/offers';
 import {useAppDispatch} from '../../store/hooks/useAppDispatch.ts';
 import {MouseEvent} from 'react';
 
 export default function LocationList(): JSX.Element {
   const currentCity = useAppSelector(offersSelector.city);
   const dispatch = useAppDispatch();
+
   const onCityClickHandler = (
     evt: MouseEvent<HTMLElement>,
-
+    cityName: CitiesName
   ) => {
     evt.preventDefault();
-    // const value = evt.target.value;
+    dispatch(offersAction.changeCity(cityName));
   };
 
   return (
@@ -23,10 +24,10 @@ export default function LocationList(): JSX.Element {
       <section className="locations container">
         <ul className="locations__list tabs__list">
           {CITIES.map((city) => (
-            <li key={city.name} className="locations__item">
+            <li key={city.id} className="locations__item">
               <Link className={clsx('locations__item-link tabs__item', {
                 'tabs__item--active': city.name === currentCity
-              })} to={AppRoute.Root} onClick={() => console.log('pushed button change city')}
+              })} to={AppRoute.Root} onClick={ (evt) => onCityClickHandler(evt, city.name)}
               >
                 <span>{city.name}</span>
               </Link>
