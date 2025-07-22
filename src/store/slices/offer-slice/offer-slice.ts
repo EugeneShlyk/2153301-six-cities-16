ff7340ceb1af3deafd00b2ed5609e4e6a71b5c44
Eugene Shlyk <serpupstars@gmail.com>
2025-07-23 07:19:18 +1000

    23.07 утро

--- src/store/slices/offer-slice/offer-slice.ts
+++ src/store/slices/offer-slice/offer-slice.ts
@@ -1,10 +1,29 @@
-import {Offer} from '@customType/offer.ts';
+import {Offer, OfferPreview} from '@customType/offer.ts';
 import {PayloadAction, createSlice} from '@reduxjs/toolkit';
-import {OFFERS_SLICE_NAME} from '@slices/slice-name.ts';
+import {OFFER_SLICE_NAME} from '@slices/slice-name.ts';
+import {OffersStateT} from '@slices/offers/types.ts';
 
 type OfferState = {
   info: Offer | null;
-  nearby: Offer[];
+  nearby: OfferPreview[];
 }
 
+const initialState: OfferState = {
+  info: null,
+  nearby: [],
+};
 
+export const offerSlice = createSlice({
+  name: OFFER_SLICE_NAME,
+  initialState,
+  reducers: {
+    clear: (state: OfferState) => {
+      state.info = null;
+      state.nearby = [];
+    },
+  },
+  selectors: {
+    nearbyOffers: (state: OffersStateT) => state.nearby,
+    offer: (state: OffersStateT) => state.info,
+  }
+});
