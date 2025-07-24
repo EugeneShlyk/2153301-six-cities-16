2ac39969b7a661847d69fa83e2f0675510fe3ed4
Eugene Shlyk <serpupstars@gmail.com>
2025-07-25 07:21:30 +1000

    25.07 утро

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
 import offersSlice from './slices/offers';
 import {rootReducer} from '@store/root-reducers.ts';
+import {createAPI} from '@shared/api.ts';
+
+const api = createAPI();
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     thunk: {
-      extraArgument:
+      extraArgument: api,
     }
   }),
 });
