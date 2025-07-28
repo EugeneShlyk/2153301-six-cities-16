4ef3af13c5832208e915c1d309498262af5721f2
Eugene Shlyk <serpupstars@gmail.com>
2025-07-28 17:16:58 +1000

    28/07/2025

--- src/store/slices/offers/offers-thunk.ts
+++ src/store/slices/offers/offers-thunk.ts
@@ -3,5 +3,5 @@ import {OFFERS_SLICE_NAME} from '@slices/slice-name.ts';
 
 
 export const fetchOffersAction = createAsyncThunk(`${OFFERS_SLICE_NAME}/fetchOffers`, async () => {
-  const data = await fetch(`${OFFERS_SLICE_NAME}/offers`, {})
-})
+  const data = await fetch(`${OFFERS_SLICE_NAME}/offers`, {});
+});
