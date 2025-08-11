c1a2d158ec23814ba8fc2a4f3f0e7a7d2ebfa063
Eugene Shlyk <serpupstars@gmail.com>
2025-08-12 07:30:50 +1000

    12.08 утро

--- src/store/slices/offers/offers-thunk.ts
+++ src/store/slices/offers/offers-thunk.ts
@@ -1,7 +1,11 @@
-import {createAsyncThunk} from '@reduxjs/toolkit';
 import {OFFERS_SLICE_NAME} from '@slices/slice-name.ts';
+import {OfferPreview} from '@customType/offer.ts';
+import {createAppAsyncThunk} from '@store/hooks/createAppAsyncThunk.ts';
 
 
-export const fetchOffersAction = createAsyncThunk(`${OFFERS_SLICE_NAME}/fetchOffers`, async () => {
-  const data = await fetch(`${OFFERS_SLICE_NAME}/offers`, {});
-});
+export const fetchOffersAction = createAppAsyncThunk<OfferPreview[], void>(
+  `${OFFERS_SLICE_NAME}/fetchOffers`,
+  async (_, {extra: api}) => {
+    const {data} = await api.get<OfferPreview[]>('/offers');
+    return data;
+  });
