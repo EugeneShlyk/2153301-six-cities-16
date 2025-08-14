1c250affa849d2a47ac1e5dda11915cb4425ce0e
Eugene Shlyk <serpupstars@gmail.com>
2025-08-14 16:04:36 +1000

    14/08/2025

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -4,10 +4,18 @@ import LocationList from '@components/location-list';
 import PlacesListSection from '@components/places-list-section';
 import {useAppSelector} from '@store/hooks/useAppSelector.ts';
 import {offersSelector} from '@slices/offers';
+import {useAppDispatch} from '@store/hooks/useAppDispatch.ts';
+import {useEffect} from 'react';
+import {fetchOffersAction} from '@slices/offers/offers-thunk.ts';
 
 function MainPage(): JSX.Element {
   const currentCity = useAppSelector(offersSelector.city);
   const offers = useAppSelector(offersSelector.offers);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchOffersAction());
+  }, []);
   const offersCurrentCity: OfferPreview[] = offers.filter((offer: OfferPreview): boolean => offer.city.name === currentCity);
 
   return (
