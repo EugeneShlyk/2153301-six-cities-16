cc23c2ed75075ec2528bd6e482951f17e3d2892c
Eugene Shlyk <serpupstars@gmail.com>
2026-02-17 07:34:23 +1000

    debgeg

--- src/store/slices/offers/offers-thunk.ts
+++ src/store/slices/offers/offers-thunk.ts
@@ -8,6 +8,5 @@ export const fetchOffersAction = createAppAsyncThunk<OfferPreview[], void, { ext
   `${OFFERS_SLICE_NAME}/fetchOffers`,
   async (_, {extra: api,}) => {
     const {data} = await api.get<OfferPreview[]>(ENDPOINTS.OFFERS);
-    console.log(data);
     return data;
   });
