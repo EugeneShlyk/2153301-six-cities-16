ae91296c07d08a4f85236c37b37ac8b9d0210897
Eugene Shlyk <serpupstars@gmail.com>
2026-03-09 07:05:52 +1000

    ьолывпдльывп

---
+++ src/components/header/header-navigation-auth.tsx
@@ -0,0 +1,22 @@
+import {Link} from 'react-router-dom';
+
+export const HeaderNavigationAuth = () => {
+  return (
+    <>
+      <li className="header__nav-item user">
+        <Link className="header__nav-link header__nav-link--profile" href="#">
+          <div className="header__avatar-wrapper user__avatar-wrapper">
+          </div>
+          <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+          <span className="header__favorite-count">3</span>
+        </Link>
+      </li>
+      <li className="header__nav-item">
+        <span className="header__nav-link">
+          <span className="header__signout">Sign out</span>
+        </span>
+      </li>
+      ;
+    </>
+  );
+};
