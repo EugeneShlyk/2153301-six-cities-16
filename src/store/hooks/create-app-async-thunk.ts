93802003bf3c88d17428c270f431a622dd09b130
Eugene Shlyk <serpupstars@gmail.com>
2026-03-13 07:16:28 +1000

    начинает знакомство с дебаггером

--- src/store/hooks/create-app-async-thunk.ts
+++ src/store/hooks/create-app-async-thunk.ts
@@ -1,6 +1,6 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AxiosInstance} from 'axios';
-import {AppDispatch, RootState} from '@store/index.ts';
+import type {AppDispatch, RootState} from '@store/index.ts';
 
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
   extra: AxiosInstance;
